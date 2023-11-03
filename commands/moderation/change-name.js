7dab23a7e11d2063cd96ffe2940c4142964059f6
Lilyana Ivanova <lilyanaivanova2118@gmail.com>
2023-11-03 19:03:32 +0200

    added nodemon

--- commands/moderation/change-name.js
+++ commands/moderation/change-name.js
@@ -6,6 +6,6 @@ module.exports = {
     .setDescription("Changes student name in server"),
   async execute(interaction) {
     console.log("Ayo what happened");
-    await interaction.reply("I don't know your name, bro");
+    await interaction.reply("I don't know your name, bro!");
   },
 };
