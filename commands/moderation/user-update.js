a7941a609b5b4a5457ac78259d62c7fb7d9dc32c
Andrian Dimitrov <134285460+AndrianDimitrov@users.noreply.github.com>
2023-11-10 21:30:20 +0200

    added role change user-update.js

--- commands/moderation/user-update.js
+++ commands/moderation/user-update.js
@@ -1,14 +1,24 @@
 const { SlashCommandBuilder } = require("discord.js");
 const userMockData = require("../../data/mock-user-data.json");
 
-
-
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user-update")
     .setDescription("Updates student information in server"),
   async execute(interaction) {
     await interaction.member.setNickname(`${userMockData.firstname} ${userMockData.lastname}`);
-    await interaction.reply("Your server name was changed!");
+
+    const cohortYear = userMockData.cohort.split('/').pop().trim();
+    const roleName = `iss${cohortYear}`;
+
+    const role = interaction.guild.roles.cache.find(r => r.name === roleName);
+
+    if (role) {
+      await interaction.member.role.add(role);
+      await interaction.reply(`Your server name was changed and role ${roleName} was assigned!`);
+    }
+     else {
+      await interaction.reply(`Your server name was changed, but role ${roleName} was not found.`);
+    }
   },
 };
