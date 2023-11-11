6b468e2d1c1862a11f2998eb94d106f9a2873448
LilyanaIvanova <lilyanaivanova2118@gmail.com>
2023-11-11 22:56:22 +0200

    Extracted feature functionality into separate functions.

--- commands/moderation/user-update.js
+++ commands/moderation/user-update.js
@@ -1,24 +1,17 @@
 const { SlashCommandBuilder } = require("discord.js");
 const userMockData = require("../../data/mock-user-data.json");
+const changeName = require("../../utils/change-name");
+const changeRole = require("../../utils/change-role");
+
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("user-update")
     .setDescription("Updates student information in server"),
   async execute(interaction) {
-    await interaction.member.setNickname(`${userMockData.firstname} ${userMockData.lastname}`);
+    await changeName(interaction, userMockData.firstname, userMockData.lastname);
+    await changeRole(interaction, userMockData.cohort)
 
-    const cohortYear = userMockData.cohort.split('/').pop().trim();
-    const roleName = `iss${cohortYear}`;
-
-    const role = interaction.guild.roles.cache.find(r => r.name === roleName);
-
-    if (role) {
-      await interaction.member.role.add(role);
-      await interaction.reply(`Your server name was changed and role ${roleName} was assigned!`);
-    }
-     else {
-      await interaction.reply(`Your server name was changed, but role ${roleName} was not found.`);
-    }
+    await interaction.reply(`Your server information was updated!`)
   },
 };
