ff668232c2c7b6aab2c965659b5d5ab64ce4710b
fORTAKENN <33179005+fORTAKENN@users.noreply.github.com>
2023-12-09 16:43:39 +0200

    Script create
    Направен е скирпт на който може да се ъплоудне .xlsx file и след тва той може да бъде конвертиран в .csv и форматиран във формата на MoodleUserImport, след това може файла да се добави в Moodle от админ панела

---
+++ Moodle/Student.cs
@@ -0,0 +1,37 @@
+﻿using XML_Converter.Contracts;
+
+namespace XML_Converter
+{
+    public class Student : IStudent
+    {
+        public string Username { get; private set; }
+
+        public string FirstName { get; private set; }
+
+        public string MiddleName { get; private set; }
+
+        public string LastName { get; private set; }
+
+        public string Email { get; private set; }
+
+        public string Password { get; private set; }
+
+        public string Cohort { get; private set; }
+
+        public Student(string username, string firstname, string middlename, string lastname, string email, string password, string cohort)
+        {
+            this.Username = username;
+            this.FirstName = firstname;
+            this.MiddleName = middlename;
+            this.LastName = lastname;
+            this.Email = email;
+            this.Password = password;
+            this.Cohort = cohort;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Username},{this.FirstName},{this.MiddleName},{this.LastName},{this.Email},{this.Password},{this.Cohort}";
+        }
+    }
+}
