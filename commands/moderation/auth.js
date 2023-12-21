ffdce727cff443d01f706d226f2833a5199e5e11
Andrian Dimitrov <134285460+AndrianDimitrov@users.noreply.github.com>
2023-12-21 13:32:46 +0200

    Update auth.js with firebase

--- commands/moderation/auth.js
+++ commands/moderation/auth.js
@@ -1,20 +1,31 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder } = require('discord.js');
+const firebaseAdmin = require('firebase-admin');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('auth')
-    .setDescription('Authenticate with your Moodle account'),
+        .setName('auth')
+        .setDescription('Authenticate with your Moodle account or get your Moodle ID'),
     async execute(interaction) {
-      const moodleAuthUrl = `http://165.232.65.76/local/oauth/login.php?client_id=moodle_fpmi&redirect_uri=http://165.232.65.76/admin/oauth2callback.php&response_type=code`;
-      try {
-        await interaction.reply({ content: `Please authenticate with Moodle, using this link: ${moodleAuthUrl}`, ephemeral: true });
-      } catch (error) {
-        console.error("Error executing auth command:", error);
-        if (error.message.includes("Missing Permissions")) {
-          await interaction.reply('I do not have permission to update this user.');
-        } else {
-          await interaction.reply('There was an error while executing this command!');
+        const discordUserId = interaction.user.id; 
+        const db = firebaseAdmin.firestore();
+
+        try {
+            const docRef = db.collection('students').doc(discordUserId);
+            const doc = await docRef.get();
+
+            if (doc.exists && doc.data().moodle_id) {
+                // The document exists and has a moodle_id, so reply
+                const moodleProfileUrl = `http://165.232.65.76/user/profile.php?id=${doc.data().moodle_id}`;
+                 await interaction.reply({ content: `Click here to view your Moodle profile: ${moodleProfileUrl}`, ephemeral: true });
+
+            } else {
+                // If no Moodle ID is found, provide instructions to log in
+                const moodleLoginUrl = `http://165.232.65.76/login`;
+                await interaction.reply({ content: `Please log in to Moodle using this link: ${moodleLoginUrl}, and after that run the command again`, ephemeral: true });
+            }
+        } catch (error) {
+            console.error("Error in Moodle command:", error);
+            await interaction.reply({ content: `There was an error processing your request.`, ephemeral: true });
         }
-      }
     },
-  };
+};
