3ceaff611731adf3194bd7964df32b7d16520988
Andrian Dimitrov <134285460+AndrianDimitrov@users.noreply.github.com>
2024-01-07 20:30:20 +0200

    Update auth command

--- commands/moderation/auth.js
+++ commands/moderation/auth.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder, MessageActionRow, MessageButton } = require('discord.js');
+const firebaseAdmin = require('firebase-admin');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -6,22 +7,42 @@ module.exports = {
         .setDescription('Authenticate with your Moodle account'),
 
     async execute(interaction) {
-        const oauthUrl = `http://165.232.65.76/local/oauth/login.php?client_id=moodle_fpmi&response_type=code`;
+        const discordUserId = interaction.user.id;
+        const userRef = firebaseAdmin.firestore().collection('students').doc(discordUserId);
 
-        
-        const row = new MessageActionRow()
-            .addComponents(
-                new MessageButton()
-                    .setLabel('Login to Moodle')
-                    .setStyle('LINK')
-                    .setURL(oauthUrl) 
-            );
+        try {
+            const doc = await userRef.get();
+            if (doc.exists && doc.data().code) {
+                const moodleAccessToken = doc.data().code;
+                const moodleLoggedInUrl = `http://165.232.65.76/your_logged_in_page?access_token=${moodleAccessToken}`;
 
-        
-        await interaction.reply({ 
-            content: 'Click the button below to log in to Moodle:', 
-            components: [row], 
-            ephemeral: true 
-        });
+                await interaction.reply({ 
+                    content: `You're already authenticated. Click here to access your Moodle site: ${moodleLoggedInUrl}`,
+                    ephemeral: true
+                });
+            } else {
+                // User needs to authenticate
+                const authUrl = `http://localhost:3000/auth?userId=${discordUserId}`;
+                const row = new MessageActionRow()
+                    .addComponents(
+                        new MessageButton()
+                            .setLabel('Authenticate with Moodle')
+                            .setStyle('LINK')
+                            .setURL(authUrl)
+                    );
+
+                await interaction.reply({ 
+                    content: 'Please click the button below to log in to Moodle and authenticate:',
+                    components: [row],
+                    ephemeral: true
+                });
+            }
+        } catch (error) {
+            console.error('Error accessing Firebase:', error);
+            await interaction.reply({ 
+                content: 'There was an error while trying to authenticate you with Moodle. Please try again later.',
+                ephemeral: true
+            });
+        }
     },
 };
